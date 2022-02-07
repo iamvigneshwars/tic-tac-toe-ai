c04e3b41ee8b61c3966e3ea2403ffa0ab67bd77d
iamvigneshwars <iamvigneshwars@gmail.com>
2022-02-07 21:34:04 +0000

    Update

--- static/js/script.js
+++ static/js/script.js
@@ -1,5 +1,5 @@
-const X_CLASS = 'x'
-const CIRCLE_CLASS = 'circle'
+const humanPlayer = 'x'
+const AiPlayer = 'o'
 const WINNING_COMBINATIONS = [
   [0, 1, 2],
   [3, 4, 5],
@@ -15,22 +15,71 @@ var Board;
 const cellElements = document.querySelectorAll('.cell')
 startGame();
 
-
 function startGame(){
     Board = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    document.getElementById("result_message").innerHTML = "Can you beat the AI?"
     for (var i = 0; i < cellElements.length; i++){
         cellElements[i].classList.remove('x');
         cellElements[i].classList.remove('o');
-        cellElements[i].addEventListener('click', turnClick);
+        cellElements[i].addEventListener('click', playMove, true);
     }
 }
 
-function turnClick(cell){
-    turn(cell.target.id, X_CLASS);
+function playMove(cell){
+    
+    if (typeof Board[cell.target.id] == 'number'){
+        turn(cell.target.id, humanPlayer);
+        let gameWon =  checkWinner(Board, humanPlayer);
+        if (gameWon) gameOver(humanPlayer);
+        if (!gameWon && !checkTie()) turn(AiMove(), AiPlayer);
+        gameWon = checkWinner(Board, AiPlayer);
+        if (gameWon) gameOver(AiPlayer);
+    }
 }
 
 function turn(cellId, player){
     Board[cellId] = player;
     document.getElementById(cellId).classList.add(player);
-    // console.log(cellId)
+}
+
+function AiMove(){
+    return emptyCells()[0];
+}
+
+function emptyCells() {
+    return Board.filter(s => typeof s == 'number');
+}
+
+function checkWinner(board, player){
+    let plays = board.reduce((a, e, i) => 
+    (e === player) ? a.concat(i) : a, []);
+	for (let [index, win] of WINNING_COMBINATIONS.entries()) {
+		if (win.every(elem => plays.indexOf(elem) > -1)) {
+            return true;
+		}
+	}
+	return false;
+}
+
+function checkTie(){
+    if (emptyCells().length == 0) {
+        for (var i = 0 ; i < cellElements.length; i++){
+            cellElements[i].removeEventListener('click', playMove, false);
+        }
+        gameOver("TIE")
+        return true;
+    }
+    return false;
+}
+
+function gameOver(gameWon){
+    for (var i = 0; i < cellElements.length; i++){
+        cellElements[i].removeEventListener('click', playMove, true);
+    }
+    console.log(gameWon)
+    let result;
+    if (gameWon == 'x') result = "You Win!";
+    else if (gameWon == 'o') result = "You Lose";
+    else result = "Ah,.. that's a tie";
+    document.getElementById("result_message").innerHTML = result;
 }
